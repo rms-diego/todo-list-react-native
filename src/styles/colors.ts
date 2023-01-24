03211f8b60e863dd7282bd578c6299df2ebe211a
Diego Ramos <diegorbrito9@gmail.com>
2023-01-24 02:18:44 -0300

    new: mapeando as cores do projeto

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  purple: "#8284FA",
+  darkPurple: "#5E60CE",
+  blue: "#4EA8DE",
+  darkBlue: "#1E6F9E",
+
+  danger: "#E25858",
+
+  "gray-100": "#F2F2F2",
+  "gray-200": "#D9D9D9",
+  "gray-300": "#808080",
+  "gray-400": "#333333",
+  "gray-500": "#262626",
+  "gray-600": "#1A1A1A",
+  "gray-700": "#0D0D0D",
+};
