1deed5517cca17ae861a640f10d6f5e6aefed4ea
Diego Ramos <diegorbrito9@gmail.com>
2023-01-24 02:27:21 -0300

    feat: criando a pagina 'home' e seu estilo inicial

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../styles/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS["gray-600"],
+    height: "100%",
+  },
+});
